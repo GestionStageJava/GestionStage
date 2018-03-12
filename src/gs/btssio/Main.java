dbd2496533cb8ae4314cf80095fbf0b3598ae8b9
Victor Dittiere <34861901+Microrad@users.noreply.github.com>
2018-03-12 17:16:29 +0100

    Ajout Microrad

--- src/gs/btssio/Main.java
+++ src/gs/btssio/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world MiSTER NOOB");
+		system.out.println("Hello, I am Microrad");
 	}
 
 }
