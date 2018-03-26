9698d0705624831a59ce47428b80f062e710a5f8
garrona <garrona@sio-l103-07.stsio.lan>
2018-03-26 15:46:40 +0200

    ajout controller boutons EditStagiaire

---
+++ PPE-stage-java/src/com/btssio/controller/ControlBtnAnnulerEditStagiaire.java
@@ -0,0 +1,17 @@
+package com.btssio.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlBtnAnnulerEditStagiaire implements ActionListener
+{
+	// EditStagiaire dialogEditEleve
+	// Creer constructeur ControlBtnValiderEditStagiaire	
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		// retour à l'acceuil de l'appli
+	}
+
+}
---
+++ PPE-stage-java/src/com/btssio/controller/ControlBtnValiderEditStagiaire.java
@@ -0,0 +1,21 @@
+package com.btssio.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlBtnValiderEditStagiaire implements ActionListener 
+{
+	//EditStagiaire dialogEditEleve
+	//Creer constructeur ControlBtnValiderEditStagiaire	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		//recuperation des infos du "formulaire"
+		//met à jour Stagiaire
+	}
+	
+	
+	
+	
+}
