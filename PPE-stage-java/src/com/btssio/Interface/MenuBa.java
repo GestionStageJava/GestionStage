d1d4aa7b30bae01fca238efd3e9c7d40909efd3e
LEVELU Maxence <levelum@SIO-103-xx.stsio.lan>
2018-04-23 15:29:21 +0200

    modif menubar

--- PPE-stage-java/src/com/btssio/Interface/MenuBa.java
+++ PPE-stage-java/src/com/btssio/Interface/MenuBa.java
@@ -7,10 +7,15 @@ import javax.swing.JMenuItem;
 import com.btssio.controller.Cmenu;
 
 public class MenuBa extends JMenuBar{
+
 		//Menu
-	private JMenu m1 = new JMenu();
-	private JMenu m2 = new JMenu();
-	private JMenu m3 = new JMenu();
+
+	private JMenu m1 = new JMenu("Accueil");
+	private JMenu m2 = new JMenu("Eleve");
+	private JMenu m3 = new JMenu("Entreprise");
+
+		//MenuItem
+
 	private JMenuItem menuItemListEleve = new JMenuItem("Lister");
 	private JMenuItem menuItemEditEleve = new JMenuItem("Editer");
 	private JMenuItem menuItemCreateEleve = new JMenuItem("Creer");
@@ -22,34 +27,53 @@ public class MenuBa extends JMenuBar{
 		
 		//menuItemListEleve.addActionListener(new CMenu());
 		//menu.addActionListener(new ClicValiderEditEleve(this));
-			//Config Menu
-			//m1
-		this.m1.setText("Accueil");
-		this.m1.addActionListener(new Cmenu(this));
-		
+
+			//Config Menu, add menuitem and setActionCommand 
+			
 			//m2
-		this.m2.setText("Eleve");
-		
+
 		this.m2.add(menuItemListEleve);
-		this.menuItemListEleve.addActionListener(new Cmenu(this));
+		this.menuItemListEleve.setActionCommand("Liste eleve");
 		
 		this.m2.add(menuItemEditEleve);
-		this.menuItemEditEleve.addActionListener(new Cmenu(this));
+		this.menuItemEditEleve.setActionCommand("Edit eleve");
+		
 
 		this.m2.add(menuItemCreateEleve);
-		this.menuItemCreateEleve.addActionListener(new Cmenu(this));
-
-			//m3
-		this.m3.setText("Entreprise");
+		this.menuItemCreateEleve.setActionCommand("Create eleve");
+		
+		
+		//m3
 		
 		this.m3.add(menuItemListEntr);
-		this.menuItemListEntr.addActionListener(new Cmenu(this));
+		this.menuItemListEntr.setActionCommand("Liste entreprise");
+		
 		
 		this.m3.add(menuItemEditEntr);
-		this.menuItemEditEntr.addActionListener(new Cmenu(this));
+		this.menuItemEditEntr.setActionCommand("Edit entreprise");
+		
 		
 		this.m3.add(menuItemCreateEntr);
+		this.menuItemCreateEntr.setActionCommand("Create entreprise");
+		
+		//Menu add action listener
+
+		//m1
+
+		this.m1.addActionListener(this);
+		
+		//m2
+
+		this.menuItemListEleve.addActionListener(new Cmenu(this));
+		this.menuItemEditEleve.addActionListener(new Cmenu(this));
+		this.menuItemCreateEleve.addActionListener(new Cmenu(this));
+
+		//m3
+
+		this.menuItemListEntr.addActionListener(new Cmenu(this));
+		this.menuItemEditEntr.addActionListener(new Cmenu(this));
 		this.menuItemCreateEntr.addActionListener(new Cmenu(this));
+
 
 			//Ajout label au menu
 		this.add(this.m1);
